35907cb63b7d4bc3cb15960a3aae28ba5afeff5f
suriyac <suriya43426@hotmail.com>
2020-09-08 14:53:12 +0700

    Enter

--- Lesson27_Tracking_Obj.py
+++ Lesson27_Tracking_Obj.py
@@ -23,13 +23,16 @@ cv2.createTrackbar('valHigh','Trackbars',255,255,nothing)
 dispW=640
 dispH=480
 flip=2
+
 #Uncomment These next Two Line for Pi Camera
+
 camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 cam= cv2.VideoCapture(camSet)
 
 #Or, if you have a WEB cam, uncomment the next line
 #(If it does not work, try setting to '1' instead of '0')
 #cam=cv2.VideoCapture(0)
+
 while True:
     ret, frame = cam.read()
     #frame=cv2.imread('smarties.png')
@@ -78,5 +81,6 @@ while True:
 
     if cv2.waitKey(1)==ord('q'):
         break
+    
 cam.release()
 cv2.destroyAllWindows()
