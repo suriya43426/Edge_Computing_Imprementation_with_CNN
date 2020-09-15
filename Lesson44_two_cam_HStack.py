db5cdb113523b8f1fddfe1ed41a2de52405d78ce
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2020-09-15 09:53:17 +0700

    Enter

---
+++ Lesson44_two_cam_HStack.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+print(cv2.__version__)
+
+dispW=640
+dispH=480
+flip=2
+dtav=0
+
+#(If it does not work, try setting to '1' instead of '0')
+cam1=cv2.VideoCapture(0)
+cam2=cv2.VideoCapture(1)
+startTime=time.time()
+
+while True:
+    ret, frame1 = cam1.read()
+    ret, frame2 = cam2.read()
+
+    frame2=cv2.resize(frame,(640,480))
+    frameCombined=np.hstack((frame1,frame2))
+
+    cv2.imshow('Combo',frameCombined)
+    #cv2.imshow('Combo',frame2)
+
+    cv2.moveWindow('Combo',0,0)
+    #cv2.moveWindow('webCam',0,500) #(high,width)
+
+    if cv2.waitKey(1)==ord('q'):
+        break
+    
+cam.release()
+cv2.destroyAllWindows()
