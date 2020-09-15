e44c2c9da4dd201716104f16658373b39b9dee70
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2020-09-15 11:59:09 +0700

    Enter

--- Lesson45_python_thread.py
+++ Lesson45_python_thread.py
@@ -6,6 +6,32 @@
 
 from threading import Thread
 import time
-def BigBox():
-    while True:
 
+def BigBox(color,l):
+    while True:
+        print(color,'Big Box is Open')
+        time.sleep(5)
+        print(color,'Big Box is Closed')
+        time.sleep(5)
+
+def SmallBox(color,l):
+    while True:
+        print(color,'Small Box is Open')
+        time.sleep(1)
+        print(color,'Small Box is Closed')
+        time.sleep(1)
+c='red'
+x=5
+bigBoxThread=Thread(target=BigBox, args=(c,x))
+c='blue'
+x=4
+smallBoxThread=Thread(target=SmallBox, args=(c,x))
+
+bigBoxThread.daemon=True
+smallBoxThread.daemon=True
+
+bigBoxThread.start()
+smallBoxThread.start()
+
+while True:
+    pass
