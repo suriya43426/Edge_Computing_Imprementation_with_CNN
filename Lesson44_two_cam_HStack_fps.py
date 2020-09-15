02915f2dabafa1dc3a03f49d8226151ff45a0271
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2020-09-15 10:05:52 +0700

    Enter

--- Lesson44_two_cam_HStack_fps.py
+++ Lesson44_two_cam_HStack_fps.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+import time
 print(cv2.__version__)
 
 dispW=640
 dispH=480
 flip=2
+font=cv2.FONT_HERSHEY_SIMPLEX
 dtav=0
 
 #(If it does not work, try setting to '1' instead of '0')
@@ -15,15 +17,18 @@ startTime=time.time()
 while True:
     ret, frame1 = cam1.read()
     ret, frame2 = cam2.read()
-    
-    frame2=cv2.resize(frame,(640,480))
+
+    frame2=cv2.resize(frame2,(frame1.shape[1],frame1.shape[0]))     #frame2=cv2.resize(frame,(640,480))
     frameCombined=np.hstack((frame1,frame2))
 
-    cv2.imshow('Combo',frameCombined)
-    #cv2.imshow('Combo',frame2)
+    dt=time.time()-startTime
+    startTime=time.time()
 
-    cv2.moveWindow('Combo',0,0)
-    #cv2.moveWindow('webCam',0,500) #(high,width)
+    dtav=.9*dtav+.1*dt
+    fps=1/dtav
+
+    cv2.imshow('Combo',frameCombined)              #cv2.imshow('Combo',frame2)
+    cv2.moveWindow('Combo',0,0)                    #cv2.moveWindow('webCam',0,500) #(high,width)
 
     if cv2.waitKey(1)==ord('q'):
         break
