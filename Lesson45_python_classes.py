b423332c4294b24c96e3995cf169045e4a61ddeb
suriya chayatummagoon <42575704+suriya43426@users.noreply.github.com>
2020-09-15 11:06:24 +0700

    Enter

--- Lesson45_python_classes.py
+++ Lesson45_python_classes.py
@@ -3,19 +3,37 @@
 #Methods and Threading
 
 class Rectangle:
-    def __init__(self,color,w,l):
+    def __init__(self,c,w,l):
         self.color=c
         self.width=w
         self.length=l
         #self.area=self.width*self.length
+    def area(self):
+        self.area=self.width*self.length
+        return self.area
+    def per(self):
+        self.perimeter=2*self.width+2*self.length
+        #กว้างx2 + ยาวx2
+        return self.perimeter
         
 c1='red'
 w1=3
 l1=4
-
 rect1=Rectangle(c1,w1,l1)
-print('Rectangle 1 is ',rect1.color,'with area', rect1.area)
+areaRect1=rect1.area()
+print(areaRect1)
 
+c2='blue'
+w2=6
+l2=7
+rect2=Rectangle(c2,w2,l2)
+areaRect2=rect2.area()
+print(areaRect2)
+
+per1=rect1.per()
+
+print('Rectangle 1 has perimeter: ', rect1.color)
 #----------------------------------
 #----------------------------------
+#perimeter เส้นรอบรูป 
 
