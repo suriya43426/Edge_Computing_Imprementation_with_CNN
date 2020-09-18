62892e7a533a4f4b4b6f4b2d3ec0a3b7edb8d2a6
suriyac <suriya43426@hotmail.com>
2020-09-18 13:55:41 +0700

    Ener

--- 1_web_camera.py
+++ 1_web_camera.py
@@ -16,7 +16,9 @@ cam=cv2.VideoCapture(0)
 while True:
     ret, frame = cam.read()
     cv2.imshow('nanoCam',frame)
+    
     if cv2.waitKey(1)==ord('q'):
         break
+
 cam.release()
 cv2.destroyAllWindows()
